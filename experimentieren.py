9a4ae83af2e1ef79982fae0a8e7c502f2fd08571
Peter Schmidt <pta@dvv32.de>
2022-02-15 15:06:34 +0100

    Änderungsvorschlag

--- experimentieren.py
+++ experimentieren.py
@@ -8,16 +8,24 @@ guesses =""
 
 while True:
     guess = input("Geben sie einen Buchstaben ein: ")
-    if len(guess) > 1:
-        print("Einen Buchstaben oder das gesamte Wort eigeben!")
-    guesses += guess
-    for char in word:
-        if char in guesses:
-            print(char,)
-        else:
-            print("_")
-    if guesses == word:
-        print("Du hast das wort erraten!")
+    if guess == word:
+        print("Du hast das Wort erraten!")
         break
     else:
-        continue
+        if len(guess) > 1:
+            print("Einen Buchstaben oder das gesamte Wort eigeben!")    
+        else:
+            guesses += guess
+            yeah = True
+            for char in word:
+                if char in guesses:
+                    print(char, end = '', flush=True)
+                else:
+                    print('_', end = '', flush=True)
+                    yeah = False
+                print()
+            if yeah:
+                print("Super!")
+                break
+
+        
